e198fd5c02ad10305a6d153b6d053d503a1b10be
Surya26668 <158288653+Surya26668@users.noreply.github.com>
2024-06-04 23:39:14 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,13 @@
+from telebot import logger
+from telebot.async_telebot import AsyncTeleBot
+from telebot.asyncio_filters import *
+from telebot.asyncio_storage import StateMemoryStorage
+from logging import ERROR
+
+bot_token = '6946010945:AAHKgdRw27gy8guQsLiS7V00g6v8BaIcTPY'
+bot = AsyncTeleBot(bot_token, parse_mode='HTML', state_storage=StateMemoryStorage())
+
+bot.add_custom_filter(StateFilter(bot))
+bot.add_custom_filter(IsDigitFilter())
+
+logger.setLevel(ERROR)
