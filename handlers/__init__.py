2a612cc42effc0d049d491b498f1ca8e8ec630ad
Surya26668 <158288653+Surya26668@users.noreply.github.com>
2024-06-06 06:32:15 +0700

    __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from bot import bot
+from message import *
 from .start import *
 from .convert import *
 from .pecahtxt import *
@@ -7,3 +10,19 @@ from .convertxlsx import *
 
 whitelist = {'6243471475': '30'} 
 print(whitelist)
+
+@bot.message_handler(commands='start')
+async def send_welcome(message):
+    try:
+        user_id = str(message.from_user.id)  # Mendapatkan user_id dari pengguna
+        if user_id in whitelist:
+            await bot.reply_to(message, txt_start)
+        else:
+            await bot.reply_to(message, "Anda tidak diizinkan untuk menggunakan bot ini.")
+    except Exception as e:
+        logging.error("error: ", exc_info=True)
+
+@bot.message_handler(state="*", commands=['cancel'])
+async def any_state(message):
+    await bot.send_message(message.chat.id, "Proses dibatalkan.")
+    await bot.delete_state(message.from_user.id, message.chat.id)
