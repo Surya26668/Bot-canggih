ece053f9bdd269ad5572909cba37c26e34f1b780
Surya26668 <158288653+Surya26668@users.noreply.github.com>
2024-06-06 17:58:33 +0700

    __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,18 +4,3 @@ from .pecahtxt import *
 from .pecahvcf import *
 from .convertvcf import *
 from .convertxlsx import *
-
-def handle_command(user_id, command):
-    if not is_whitelisted(user_id):
-        print("Pengguna tidak termasuk whitelist.")
-        return
-        
-    if command == "start":
-        start()
-    elif command == "convert":
-        convert()
-    else:
-        print("Perintah tidak dikenal")
-
-user_id = "6243471475"
-handle_command(user_id, command)
