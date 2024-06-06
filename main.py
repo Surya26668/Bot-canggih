62f92ce58a1cd19976f15b3310718e3aef5fb488
Surya26668 <158288653+Surya26668@users.noreply.github.com>
2024-06-06 19:15:50 +0700

    main.py

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ from commands import *
 from message import *
 from handlers import *
 
-whitelist = {'6243471475': '30'}
-
-def is_whitelisted(user_id):
-    return str(user_id) in whitelist
-
 async def main():
   await set_commands()
   await bot.infinity_polling()
