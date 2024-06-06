a7f4ac1abd253816d7ab433425d0283e1f8c5449
Surya26668 <158288653+Surya26668@users.noreply.github.com>
2024-06-06 11:49:48 +0700

    __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -5,22 +5,16 @@ from .pecahvcf import *
 from .convertvcf import *
 from .convertxlsx import *
 
-# Contoh fungsi penanganan perintah
 def handle_command(user_id, command):
     if not is_whitelisted(user_id):
         print("Pengguna tidak termasuk whitelist.")
         return
-
-    # Logika penanganan perintah yang ada
+        
     if command == "start":
         start()
     elif command == "convert":
         convert()
-    # Tambahkan perintah lain sesuai kebutuhan
     else:
         print("Perintah tidak dikenal")
 
-# Contoh pemanggilan
 user_id = "6243471475"
-command = "start"
-handle_command(user_id, command)
