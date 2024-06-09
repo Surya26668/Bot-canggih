07499d87528419469e6621d19ef11048f5f67587
Surya26668 <158288653+Surya26668@users.noreply.github.com>
2024-06-09 21:22:39 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telebot.asyncio_filters import *
 from telebot.asyncio_storage import StateMemoryStorage
 from logging import ERROR
 
-bot_token = '6946010945:AAHKgdRw27gy8guQsLiS7V00g6v8BaIcTPY'
+bot_token = '7391463891:AAHC2_qulTzAtsKzHe0Vmlu1YQ21fPA4LrA'
 bot = AsyncTeleBot(bot_token, parse_mode='HTML', state_storage=StateMemoryStorage())
 
 bot.add_custom_filter(StateFilter(bot))
