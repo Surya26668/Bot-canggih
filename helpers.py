c9560ce6b7b7891874a7cccfa0dc5badd0c565a6
RAT <158288653+Surya26668@users.noreply.github.com>
2024-08-30 15:02:42 +0700

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,153 +1,149 @@
 import pandas as pd
 
 def convert(data):
-  numbers = check_number(data['filename'])
-  split_number = split(numbers, data['totalc'])
+    numbers = check_number(data['filename'])
+    split_number = split(numbers, data['totalc'])
 
-  countc = 0
-  countf = 0
-  vcf_files = []
-  sisa = []
-  for numbers in split_number:
-    vcard_entries = []
-    for number in numbers:
-      countc+=1
-      vcard_entry = f"BEGIN:VCARD\nVERSION:3.0\nFN:{data['cname']}-{countc}\nTEL;TYPE=CELL:+{number}\nEND:VCARD"
-      vcard_entries.append(vcard_entry)
+    countc = 0
+    countf = 0
+    vcf_files = []
+    sisa = []
+    
+    for numbers in split_number:
+        vcard_entries = []
+        for number in numbers:
+            countc += 1
+            vcard_entry = f"BEGIN:VCARD\nVERSION:3.0\nFN:{data['cname']}-{countc}\nTEL;TYPE=CELL:+{number}\nEND:VCARD"
+            vcard_entries.append(vcard_entry)
 
-    countf+=1
-    if countf > data['totalf']:
-      sisa.append(numbers)
-    else:
-      vcf_name = f"files/{data['name']}_{countf}.vcf"
-      vcf_files.append(vcf_name)
-      
-      with open(vcf_name, 'w', encoding='utf-8') as vcard_file:
-        for entry in vcard_entries:
-            vcard_file.write(entry + "\n")
-  
-  if sisa:
-    file_txt = "files/sisa.txt"
-    vcf_files.append(file_txt)
+        countf += 1
+        if countf > data['totalf']:
+            sisa.extend(numbers)  # Use extend instead of append to flatten the list
+        else:
+            vcf_name = f"files/{data['name']}_{countf}.vcf"
+            vcf_files.append(vcf_name)
+            
+            with open(vcf_name, 'w', encoding='utf-8') as vcard_file:
+                vcard_file.write("\n".join(vcard_entries) + "\n")
     
-    with open(file_txt, 'w', encoding='utf-8') as file:
-      for s in sisa:
-        file.write("\n".join(s) + "\n")
+    if sisa:
+        file_txt = "files/sisa.txt"
+        vcf_files.append(file_txt)
+        
+        with open(file_txt, 'w', encoding='utf-8') as file:
+            file.write("\n".join(sisa) + "\n")
     
-  return vcf_files
+    return vcf_files
 
 def convert_vcf(data):
-  data['filename'] = convert_xlsx_to_txt(data)
-  numbers = check_number(data['filename'])
-  split_number = split(numbers, data['totalc'])
+    data['filename'] = convert_xlsx_to_txt(data)
+    numbers = check_number(data['filename'])
+    split_number = split(numbers, data['totalc'])
 
-  countc = 0
-  countf = 0
-  vcf_files = []
-  for numbers in split_number:
-    vcard_entries = []
-    for number in numbers:
-      countc+=1
-      vcard_entry = f"BEGIN:VCARD\nVERSION:3.0\nFN:{data['cname']}-{countc}\nTEL;TYPE=CELL:+{number}\nEND:VCARD"
-      vcard_entries.append(vcard_entry)
-
-    countf+=1
-    vcf_name = f"files/{data['name']}_{countf}.vcf"
-    vcf_files.append(vcf_name)
+    countc = 0
+    countf = 0
+    vcf_files = []
     
-    with open(vcf_name, 'w', encoding='utf-8') as vcard_file:
-      for entry in vcard_entries:
-          vcard_file.write(entry + "\n")
-    
-    if countf == data['totalf']:
-      break
+    for numbers in split_number:
+        vcard_entries = []
+        for number in numbers:
+            countc += 1
+            vcard_entry = f"BEGIN:VCARD\nVERSION:3.0\nFN:{data['cname']}-{countc}\nTEL;TYPE=CELL:+{number}\nEND:VCARD"
+            vcard_entries.append(vcard_entry)
 
-  return vcf_files
+        countf += 1
+        vcf_name = f"files/{data['name']}_{countf}.vcf"
+        vcf_files.append(vcf_name)
+        
+        with open(vcf_name, 'w', encoding='utf-8') as vcard_file:
+            vcard_file.write("\n".join(vcard_entries) + "\n")
+        
+        if countf == data['totalf']:
+            break
+
+    return vcf_files
 
 def convert_xlsx_to_txt(data):
-  df = pd.read_excel(data['filename'])
-  file_name = f"files/{data['name']}.txt"
-  df.to_csv(file_name, index=False, sep='\t')
+    df = pd.read_excel(data['filename'])
+    file_name = f"files/{data['name']}.txt"
+    df.to_csv(file_name, index=False, sep='\t')
 
-  return file_name
+    return file_name
 
 def check_number(path):
-  numbers = []
+    numbers = []
 
-  with open(path, 'r', encoding='utf-8') as file:
-    lines = file.readlines()
+    with open(path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
 
-  for line in lines:
-    line = line.strip().replace('+', '')
-    if line.isdigit():
-      numbers.append(line)
+    for line in lines:
+        line = line.strip().replace('+', '')
+        if line.isdigit():
+            numbers.append(line)
 
-  return numbers
+    return numbers
 
 def pecah_txt(data):
-  numbers = check_number(data['filename'])
-  split_number = split(numbers, data['totaln'])
-  countf = 0
-  files = []
+    numbers = check_number(data['filename'])
+    split_number = split(numbers, data['totalc'])
+    countf = 0
+    files = []
 
-  for numbers in split_number:
-    countf+=1
-    txt_name = f"files/{data['name']}_{countf}.txt"
-    files.append(txt_name)
+    for numbers in split_number:
+        countf += 1
+        txt_name = f"files/{data['name']}_{countf}.txt"
+        files.append(txt_name)
 
-    with open(txt_name, 'w', encoding='utf-8') as file:
-      for number in numbers:
-        file.write(number + "\n")
+        with open(txt_name, 'w', encoding='utf-8') as file:
+            file.write("\n".join(numbers) + "\n")
 
-    if countf == data['totalf']:
-      break
-  
-  return files
+        if countf == data['totalf']:
+            break
+    
+    return files
 
 def pecah_vcf(data):
-  with open(data['filename'], 'r', encoding='utf-8') as file:
-    lines = file.readlines()
+    with open(data['filename'], 'r', encoding='utf-8') as file:
+        lines = file.readlines()
 
-  contacts = []
-  current_contact = []
+    contacts = []
+    current_contact = []
 
-  for line in lines:
-    if not line.strip():
-      continue
+    for line in lines:
+        if not line.strip():
+            continue
 
-    current_contact.append(line)
-    if line.strip() == 'END:VCARD':
-      contacts.append(current_contact)
-      current_contact = []
+        current_contact.append(line)
+        if line.strip() == 'END:VCARD':
+            contacts.append(current_contact)
+            current_contact = []
 
-  split_contact = split(contacts, data['totalc'])
-  countf = 0
-  files = []
+    split_contact = split(contacts, data['totalc'])
+    countf = 0
+    files = []
 
-  for contacts in split_contact:
-    countf+=1
-    file_name = f"files/{data['name']}_{countf}.vcf"
-    files.append(file_name)
-    
-    with open(file_name, 'w', encoding='utf-8') as file:
-      for contact in contacts:
-        contact = "".join(contact)
-        file.write(contact)
+    for contacts in split_contact:
+        countf += 1
+        file_name = f"files/{data['name']}_{countf}.vcf"
+        files.append(file_name)
+        
+        with open(file_name, 'w', encoding='utf-8') as file:
+            file.write("".join("".join(contact) for contact in contacts))
 
-    if countf == data['totalf']:
-      break
+        if countf == data['totalf']:
+            break
 
-  return files
+    return files
 
 def split(arr, num):
-  return [arr[x:x+num] for x in range(0, len(arr), num)]
+    return [arr[x:x+num] for x in range(0, len(arr), num)]
 
 if __name__ == "__main__":
-  data = {
-    'filename': 'files/11-112.txt',
-    'name': 'tes',
-    'cname': 'tes',
-    'totalc': 100,
-    'totalf': 5,
-  }
-  convert(data)
+    data = {
+        'filename': 'files/11-112.txt',
+        'name': 'tes',
+        'cname': 'tes',
+        'totalc': 100,
+        'totalf': 5,
+    }
+    convert(data)
